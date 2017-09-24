b4da7d694db95410f03cb668806efd06401e4d15
Steven Anderson <happygoatdesigns@gmail.com>
2017-09-24 01:29:26 -0600

    fixes ham button animation to work perfectly

--- js/main.js
+++ js/main.js
@@ -51,7 +51,6 @@ socialMediaButton.addEventListener('click', function() {
 
 // NAV BUTTON ANIMATION AND REVEAL
 var navButton = document.querySelector('#ham-button');
-var navClick = false;
 var navContainer = document.querySelector('nav');
 var navSlideDown = document.querySelector('.nav-dropdown-1');
 var navSlideUp = document.querySelector('.nav-dropdown-2');
@@ -59,18 +58,23 @@ var navTint = document.querySelector('.tinted-background');
 var navTopBar = document.querySelector('.top-bar');
 var navMiddleBar = document.querySelector('.middle-bar');
 var navBottomBar = document.querySelector('.bottom-bar');
-var navTopMove = document.querySelector('.top-move');
-var navMiddleOpacity = document.querySelector('.middle-opacity');
-var navBottomMove = document.querySelector('.bottom-move');
+var navClick = false;
 
 navButton.addEventListener('click', function() {
   if (click === false) {
     click = true;
+
+    navTopBar.style.transition = 'top 200ms linear 200ms, transform 200ms ease 400ms';
+    navMiddleBar.style.transition = 'width 200ms linear, opacity 200ms ease 200ms';
+    navBottomBar.style.transition = 'width 200ms linear, bottom 200ms linear 200ms, transform 200ms ease 400ms';
+
     navMiddleBar.style.width = '24px';
     navBottomBar.style.width = '24px';
-    navMiddleOpacity.style.opacity = 0;
-    navTopMove.style.transform = 'translateY(7px) rotateZ(-45deg)';
-    navBottomMove.style.transform = 'translateY(-7px) rotateZ(45deg)';
+    navMiddleBar.style.opacity = 0;
+    navTopBar.style.top = '7px';
+    navTopBar.style.transform = 'rotateZ(-45deg)';
+    navBottomBar.style.bottom = '7px';
+    navBottomBar.style.transform = 'rotateZ(45deg)';
 
     navContainer.style.zIndex = '9000';
     navSlideDown.style.transform = 'translateY(-60px)';
@@ -78,11 +82,18 @@ navButton.addEventListener('click', function() {
     navTint.style.opacity = 1;
   } else {
     click = false;
+
+    navTopBar.style.transition = 'transform 200ms ease, top 200ms linear 200ms';
+    navMiddleBar.style.transition = 'opacity 200ms ease 200ms, width 200ms linear 400ms';
+    navBottomBar.style.transition = 'transform 200ms ease, bottom 200ms linear 200ms, width 200ms linear 400ms';
+
     navMiddleBar.style.width = '12px';
     navBottomBar.style.width = '18px';
-    navMiddleOpacity.style.opacity = 1;
-    navTopMove.style.transform = 'translateY(0px) rotateZ(0deg)';
-    navBottomMove.style.transform = 'translateY(0px) rotateZ(0deg)';
+    navMiddleBar.style.opacity = 1;
+    navTopBar.style.top = '0px';
+    navTopBar.style.transform = 'rotateZ(0deg)';
+    navBottomBar.style.bottom = '0px';
+    navBottomBar.style.transform = 'rotateZ(0deg)';
 
     navSlideDown.style.transform = 'translateY(-100vh)';
     navSlideUp.style.transform = 'translateY(100vh)';
